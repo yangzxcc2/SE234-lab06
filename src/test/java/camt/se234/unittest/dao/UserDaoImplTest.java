0d5e827cab7504af462c18de1d08c11050c620a5
Thanyalak Sunrinpun <thanyalak_su@cmu.ac.th>
2017-03-27 14:43:15 +0700

    update v.1

--- src/test/java/camt/se234/unittest/dao/UserDaoImplTest.java
+++ src/test/java/camt/se234/unittest/dao/UserDaoImplTest.java
@@ -1,15 +1,16 @@
 package camt.se234.unittest.dao;
 import camt.se234.unittest.entity.User;
 import camt.se234.unittest.exception.OldDateException;
+import camt.se234.unittest.exception.OldManException;
 import camt.se234.unittest.service.UserServiceImpl;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsInAnyOrder;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.core.IsCollectionContaining.hasItems;
 public class UserDaoImplTest {
     @Test
@@ -95,6 +96,26 @@ public class UserDaoImplTest {
                 ));
     }
     @Test
+    public void testPubAllow() {
+        List<User> list = new ArrayList<>();
+        list.add(new User("Prayuth", "1234", "Tu",
+                LocalDate.of(1979, 2, 14), "08612345678"));
+        UserServiceImpl userService = new UserServiceImpl();
+        UserDaoImpl userDao = new UserDaoImpl();
+        userService.setUserDao(userDao);
+        thrown.expect(OldManException.class);
+        assertThat(userService.getPubAllowanceUser(LocalDate.of(2017, 3, 20)), is(list));
+    }
+    @Test
+    public void testLogin() {
+        UserServiceImpl userService = new UserServiceImpl();
+        UserDaoImpl userDao = new UserDaoImpl();
+        userService.setUserDao(userDao);
+        assertThat(userService.login("Prayuth", "1234"), is(new User("Prayuth", "1234", "Tu",
+                LocalDate.of(1979, 2, 14), "08612345678")));
+        assertThat(userService.login("Abc", "1234"), is(nullValue()));
+    }
+    @Test
     public void testAbleToGoToPub() {
         UserServiceImpl userService = new UserServiceImpl();
         UserDaoImpl userDao = new UserDaoImpl();
@@ -108,12 +129,15 @@ public class UserDaoImplTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
     @Test
-    public void testLoginException(){
+    public void testLoginException() {
         UserServiceImpl userService = new UserServiceImpl();
         UserDaoImpl userDao = new UserDaoImpl();
         userService.setUserDao(userDao);
 // check for the exception we expect
         thrown.expect(NullPointerException.class);
-        userService.login("","");
+        userService.login("", "");
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage("User name must not contain special characters");
+        userService.login("abcd*", "1234");
     }
 }
